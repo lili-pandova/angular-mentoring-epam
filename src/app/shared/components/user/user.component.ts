343f4192c0cad7cb016733e88df7ed0309e9e758
Liliya_Pandova <liliya_pandova@epam.com>
2019-10-22 09:32:29 +0300

    Style footer

--- src/app/shared/components/user/user.component.ts
+++ src/app/shared/components/user/user.component.ts
@@ -1,26 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { User } from "src/app/shared/models/user/user";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.scss"]
 })
 export class UserComponent implements OnInit {
   public listUsers: User[] = [
     {
       id: 1,
-      firstName: 'Maria',
-      lastName: 'Ivanova'
+      firstName: "Maria",
+      lastName: "Ivanova"
     },
     {
       id: 3,
-      firstName: 'Krasi',
-      lastName: 'Todorova'
-    },
+      firstName: "Krasi",
+      lastName: "Todorova"
+    }
   ];
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
