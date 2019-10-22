412ff6ca3dec199a79cf5bea2ae6f8d4cb1bb9cf
Liliya_Pandova <liliya_pandova@epam.com>
2019-10-22 18:13:20 +0300

    Fixing errors-in progress

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IndexComponent } from './index/index.component';
-import { ItemComponent } from './index/item/item.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-
+import { IndexComponent } from "./index/index.component";
+import { ItemComponent } from "./index/item/item.component";
 
 @NgModule({
   declarations: [IndexComponent, ItemComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule]
 })
-export class CoursesModule { }
+export class CoursesModule {}
